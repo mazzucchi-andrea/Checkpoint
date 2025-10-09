d0c6ceedd8b958e28061638169dd7caf8917ec9a
mazzucchi-andrea <mazzucchiandrea@gmail.com>
2025-10-09 12:57:35 +0200

    asm_inline full test suite with 128 bit

--- asm_inline/full_test_suite.c
+++ asm_inline/full_test_suite.c
@@ -20,18 +20,26 @@
 #define SIZE 0x200000UL
 #endif
 
+#ifndef MOD
+#define MOD 64
+#endif
+
+#if MOD == 64
 #define BITARRAY_SIZE SIZE / 8
+#elif MOD == 128
+#define BITARRAY_SIZE SIZE / 16
+#endif
 
 #define STR1(x) #x
 #define STR(x) STR1(x)
 
 extern int arch_prctl(int code, unsigned long addr);
 
-void checkpoint_dqword(int8_t *int_ptr, int offset);
+void checkpoint(int8_t *area);
 
 void *tls_setup() {
     unsigned long addr;
-    addr = (unsigned long)mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+    addr = (unsigned long)mmap(NULL, 64, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
     *(unsigned long *)addr = addr;
     if (arch_prctl(ARCH_SET_GS, addr)) {
         return NULL;
@@ -39,10 +47,57 @@ void *tls_setup() {
     return (void *)addr;
 }
 
-inline void checkpoint_dqword(int8_t *int_ptr, int offset) {
-    int_ptr += offset;
+inline void checkpoint(int8_t *area) {
 
-    __asm__ __inline__(
+__asm__ __inline__(
+#if MOD == 64
+        "mov %%rax, %%gs:0;"
+        "mov %%rbx, %%gs:8;"
+        "mov %%rcx, %%gs:16;"
+        "mov %%rax, %%rcx;"
+        "and $0xffffffffffc00000,%%rcx;"
+        "and $0x3fffff, %%rax;"
+        "test $7, %%rax;"
+        "jz second_qword;"
+        "and $0x7ffff8, %%rax;"
+        "shr $3, %%rax;"
+        "mov %%rax, %%rbx;"
+        "and $15, %%rbx;"
+        "shr $4, %%rax;"
+        "bts %%bx, " STR(2 * SIZE) "(%%rcx, %%rax, 2);"
+        "jc next_qword;"
+        "shl $4, %%rax;"
+        "add %%rbx, %%rax;"
+        "mov (%%rcx, %%rax, 8), %%rbx;"
+        "mov %%rbx, " STR(SIZE) "(%%rcx, %%rax, 8);"
+        "jmp check_last;"
+    "next_qword:"
+        "shl $4, %%rax;"
+        "add %%rbx, %%rax;"
+    "check_last:"
+        "shl $3, %%rax;"
+        "add $8, %%rax;"
+        "cmp $" STR(SIZE) ", %%rax;"
+        "jge end;"
+    "second_qword:"
+        "shr $3, %%rax;"
+        "mov %%rax, %%rbx;"
+        "and $15, %%rbx;"
+        "shr $4, %%rax;"
+        "bts %%bx, " STR(2 * SIZE) "(%%rcx, %%rax, 2);"
+        "jc end;"
+        "shl $4, %%rax;"
+        "add %%rbx, %%rax;"
+        "mov (%%rcx, %%rax, 8), %%rbx;"
+        "mov %%rbx, " STR(SIZE) "(%%rcx, %%rax, 8);"
+    "end:"
+        "mov %%gs:0, %%rax;"
+        "mov %%gs:8, %%rbx;"
+        "mov %%gs:16, %%rcx;"
+        :
+        : "a"(area)
+        : "rbx", "rcx", "memory"
+#elif MOD == 128
         "mov %%rax, %%gs:0;"
         "mov %%rbx, %%gs:8;"
         "mov %%rcx, %%gs:16;"
@@ -57,42 +112,44 @@ inline void checkpoint_dqword(int8_t *int_ptr, int offset) {
         "mov %%rax, %%rbx;"
         "and $15, %%rbx;"
         "shr $4, %%rax;"
-        "bts %%bx, " STR(2 * SIZE) "(%%rcx, %%rax, 2);"
+        "bts %%bx, " STR(2 * SIZE)"(%%rcx, %%rax, 2);"
         "jc next_qword;"
         "shl $4, %%rax;"
         "add %%rbx, %%rax;"
         "shl $4, %%rax;"
         "movdqu (%%rcx, %%rax,), %%xmm1;"
-        "movdqu %%xmm1, " STR(SIZE) "(%%rcx, %%rax,);"
+        "movdqu %%xmm1, " STR(SIZE)"(%%rcx, %%rax,);"
         "jmp check_last;"
-        "next_qword:"
+    "next_qword:"
         "shl $4, %%rax;"
         "add %%rbx, %%rax;"
-        "check_last:"
+    "check_last:"
         "shl $4, %%rax;"
         "add $16, %%rax;"
         "cmp $" STR(SIZE) ", %%rax;"
         "jge end;"
-        "second_dqword:"
+    "second_dqword:"
         "shr $4, %%rax;"
         "mov %%rax, %%rbx;"
         "and $15, %%rbx;"
         "shr $4, %%rax;"
-        "bts %%bx, " STR(2 * SIZE) "(%%rcx, %%rax, 2);"
+        "bts %%bx, " STR(2 * SIZE)"(%%rcx, %%rax, 2);"
         "jc end;"
         "shl $4, %%rax;"
         "add %%rbx, %%rax;"
         "shl $4, %%rax;"
         "movdqu (%%rcx, %%rax,), %%xmm1;"
         "movdqu %%xmm1, " STR(SIZE) "(%%rcx, %%rax,);"
-        "end:"
+    "end:"
         "mov %%gs:0, %%rax;"
         "mov %%gs:8, %%rbx;"
         "mov %%gs:16, %%rcx;"
         "movdqu %%gs:24, %%xmm1;"
         :
-        : "a"(int_ptr)
-        : "rbx", "rcx", "memory");
+        : "a"(area)
+        : "rbx", "rcx", "xmm1", "memory"
+#endif
+);
 }
 
 /* Initialize the area with the given quadword */
@@ -108,10 +165,11 @@ void test_checkpoint(int8_t *area, int64_t new_value, int numberOfWrites, int nu
     int64_t read_value;
     clock_t begin, end;
     double time_spent;
+
     begin = clock();
     for (int i = 0; i < numberOfWrites; i += 4) {
         offset = i % (SIZE - 8 + 1);
-        checkpoint_dqword(area, offset);
+        checkpoint(area + offset);
         *(int64_t *)(area + offset) = new_value;
     }
     for (int i = 0; i < numberOfReads; i++) {
@@ -119,8 +177,9 @@ void test_checkpoint(int8_t *area, int64_t new_value, int numberOfWrites, int nu
         read_value = *(int64_t *)(area + offset);
     }
     end = clock();
+
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Time spent by %d writes and %d reads: %f s\n", numberOfWrites, numberOfReads, time_spent);
+    printf("Time spent by %d write and %d reads: %f s\n", numberOfWrites, numberOfReads, time_spent);
 }
 
 void test_no_checkpoint(int8_t *area, int64_t new_value, int numberOfWrites, int numberOfReads) {
@@ -128,17 +187,12 @@ void test_no_checkpoint(int8_t *area, int64_t new_value, int numberOfWrites, int
     int64_t read_value;
     clock_t begin, end;
     double time_spent;
+    
     begin = clock();
-    for (int i = 0; i < numberOfWrites; i += 4) {
+    for (int i = 0; i < numberOfWrites; i++) {
         offset = i % (SIZE - 8 + 1);
         *(int64_t *)(area + offset) = new_value;
     }
-    end = clock();
-
-    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Time spent by %d writes: %f s\n", numberOfWrites, time_spent);
-
-    begin = clock();
     for (int i = 0; i < numberOfReads; i++) {
         offset = i % (SIZE - 8 + 1);
         read_value = *(int64_t *)(area + offset);
@@ -146,25 +200,37 @@ void test_no_checkpoint(int8_t *area, int64_t new_value, int numberOfWrites, int
     end = clock();
 
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Time spent by %d reads: %f s\n", numberOfReads, time_spent);
+    printf("Time spent by %d write and %d reads: %f s\n", numberOfWrites, numberOfReads, time_spent);
 }
 
 /* Verify that the set bits correspond to the correctly saved quadwords. */
 int verify_checkpoint(int8_t *area, int8_t *init_A_copy) {
-    int8_t *bitmap = area + SIZE * 2;
+    int8_t *bitarray = area + SIZE * 2;
     int8_t *areaS = area + SIZE;
     for (int offset = 0; offset < BITARRAY_SIZE; offset += 2) {
-        int16_t current_word = bitmap[offset];
+        u_int16_t current_word = *(u_int16_t *)(bitarray + offset) ;
         if (current_word == 0) {
             continue;
         }
         for (int k = 0; k < 16; k++) {
             if ((current_word >> k) & 1) {
+#if MOD == 64
+                int target_offset = (offset * 8 + k) * 8;
+                if (*(u_int64_t *)(init_A_copy + target_offset) != *(u_int64_t *)(areaS + target_offset)) {
+                    fprintf(stderr,
+                            "Checkpoint verify failed:\n"
+                            "Offset 0x%x\n"
+                            "Area S value: 0x%lx\n"
+                            "Area A init Value: 0x%lx\n",
+                            offset, *(u_int64_t *)(areaS + target_offset), *(u_int64_t *)(init_A_copy + target_offset));
+                    return -1;
+                }
+#elif MOD == 128
                 int target_offset = (offset * 8 + k) * 16;
                 if (*(__int128 *)(init_A_copy + target_offset) != *(__int128 *)(areaS + target_offset)) {
                     fprintf(stderr,
                             "Checkpoint verify failed:\n"
-                            "BitArray Word Offset: %d\n"
+                            "BitArray Word Offset: 0x%x\n"
                             "Bit: %d\n"
                             "Target Offset: %d\n"
                             "Area S Value: First qword: 0x%lx Second qword: 0x%lx\n"
@@ -178,6 +244,7 @@ int verify_checkpoint(int8_t *area, int8_t *init_A_copy) {
                             *(int64_t *)(init_A_copy + target_offset + 8));
                     return -1;
                 }
+#endif
             }
         }
     }
@@ -188,27 +255,32 @@ void restore_area(int8_t *area) {
     int8_t *bitarray = area + 2 * SIZE;
     int8_t *src = area + SIZE;
     int8_t *dst = area;
-    int16_t current_word;
+    u_int16_t current_word;
     int target_offset;
     clock_t begin, end;
     double time_spent;
     begin = clock();
 
     for (int offset = 0; offset < BITARRAY_SIZE; offset += 8) {
-        if (*(int64_t *)(bitarray + offset) == 0) {
+        if (*(u_int64_t *)(bitarray + offset) == 0) {
             continue;
         }
-        for (int j = 0; j < 8; j += 2) {
-            current_word = *(int16_t *)(bitarray + offset + j);
+        for (int i = 0; i < 8; i += 2) {
+            current_word = *(u_int16_t *)(bitarray + offset + i);
             if (current_word == 0) {
                 continue;
             }
             for (int k = 0; k < 16; k++) {
-                    if ((current_word >> k) & 1) {
-                        target_offset = ((offset + j) * 8 + k) * 16;
+                if (((current_word >> k) & 1) == 1) {
+#if MOD == 64
+                    target_offset = ((offset + i) * 8 + k) * 8;
+                    *(u_int64_t *)(dst + target_offset) = *(u_int64_t *)(src + target_offset);
+#elif MOD == 128
+                    target_offset = ((offset + i) * 8 + k) * 16;
                         *(__int128 *)(dst + target_offset) = *(__int128 *)(src + target_offset);
-                    }
+#endif
                 }
+            }
         }
     }
     memset(bitarray, 0, BITARRAY_SIZE);
@@ -232,8 +304,7 @@ void clean_cache(int8_t *area) {
 int main(int argc, char *argv[]) {
     char *endptr;
     int numberOfWrites, numberOfReads, ret;
-    clock_t begin, end;
-    int64_t init_value, first_value, second_value;
+    u_int64_t init_value, first_value, second_value;
 
     if (argc < 3) {
         fprintf(stderr, "Usage: %s <numberOfWrites> <<numberOfReads>\n", argv[0]);
@@ -322,7 +393,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Area A restore check failed: 0x%x\n", ret);
         return EXIT_FAILURE;
     }
-    
+
     printf("\nRepeat the writes without checkpoint to measure the overhead\n");
     clean_cache(area);
     test_no_checkpoint(area, first_value, numberOfWrites, numberOfReads);
