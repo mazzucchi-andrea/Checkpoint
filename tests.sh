6a092f89d6d6df7419789f062e9586bc82748bfd
mazzucchi-andrea <mazzucchiandrea@gmail.com>
2025-10-10 09:18:52 +0200

    rename directory MVM

--- tests.sh
+++ tests.sh
@@ -4,7 +4,7 @@ declare -a ops=(1000 10000 100000 1000000)
 declare -a size=(0x100000UL 0x200000UL 0x400000UL)
 declare -a writes=(0.95 0.90 0.85 0.80 0.75 0.70 0.65 0.60 0.55 0.50 0.45 0.40 0.35 0.30)
 
-cd MVM
+cd MVM_CKPT
 echo "size,cache_flush,mod,ops,writes,reads,ckpt_time,restore_time" > test_results.csv
 
 for mod in 64 128 256;
