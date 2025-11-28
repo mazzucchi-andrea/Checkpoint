18036f6231dc18673da2ea5d9c18f60de674bf7c
mazzucchi-andrea <mazzucchiandrea@gmail.com>
2025-11-28 10:18:27 +0100

    regs save update

--- tests.sh
+++ tests.sh
@@ -5,34 +5,6 @@ declare -a ops=(1000 10000 100000 1000000)
 declare -a size=(0x100000UL 0x200000UL 0x400000UL)
 declare -a writes=(0.95 0.90 0.85 0.80 0.75 0.70 0.65 0.60 0.55 0.50 0.45 0.40 0.35 0.30)
 
-rm -r graphs
-
-# Tests with MVM example patch
-cd MVM
-rm mvm_test_results.csv
-echo "size,cache_flush,ops,writes,reads,time" > mvm_test_results.csv
-
-for s in ${size[@]};
-do
-    for cf in ${cache_flush[@]};
-    do
-        for o in ${ops[@]}; 
-        do
-            for w in ${writes[@]}; 
-            do
-                w_ops=$(echo "$o * $w" | bc)
-                w_ops=${w_ops%.*}
-                r_ops=$(echo "$o - $w_ops" | bc)
-                make MEM_SIZE=$s WRITES=$w_ops READS=$r_ops CF=$cf
-                ./application/prog
-            done
-        done
-    done
-done
-
-make clean
-cd ..
-
 # Tests with MVM_CKPT
 cd MVM_CKPT
 rm ckpt_test_results.csv
@@ -56,6 +28,32 @@ do
                     make ALLOCATOR_AREA_SIZE=$s WRITES=$w_ops READS=$r_ops CF=$cf MOD=$mod
                     ./application/prog
                 done
+            done
+        done
+    done
+done
+
+make clean
+cd ..
+
+# Tests with MVM example patch
+cd MVM
+rm mvm_test_results.csv
+echo "size,cache_flush,ops,writes,reads,time" > mvm_test_results.csv
+
+for s in ${size[@]};
+do
+    for cf in ${cache_flush[@]};
+    do
+        for o in ${ops[@]}; 
+        do
+            for w in ${writes[@]}; 
+            do
+                w_ops=$(echo "$o * $w" | bc)
+                w_ops=${w_ops%.*}
+                r_ops=$(echo "$o - $w_ops" | bc)
+                make MEM_SIZE=$s WRITES=$w_ops READS=$r_ops CF=$cf
+                ./application/prog
             done
         done
     done
